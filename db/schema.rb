7aa4145fd382b6d7937d7950eedbbca420ba46a1
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2022-07-01 14:50:46 +0700

    Set up Comment model & controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_01_041127) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_01_075006) do
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "friend_requests", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
