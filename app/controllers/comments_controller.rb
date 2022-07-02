69621289c8f48ddf668c7b81bec0732239a0fa2e
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2022-07-02 14:59:55 +0700

    Add edit/delete options to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,19 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to current_user, notice: 'Comment was successfully updated!'
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def comment_params
