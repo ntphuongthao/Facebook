c33c399acecbb98abea6ce9be2acb3025fb2a2a1
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2022-07-02 19:10:44 +0700

    Finished the comment for post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:destroy, :update, :edit]
   def new
     @comment = Comment.new
   end
@@ -6,28 +7,42 @@ class CommentsController < ApplicationController
   def create 
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to current_user, notice: 'Comment was successfully created!'
+      redirect_to User.find(params[:comment][:friend_id].to_i), notice: 'Comment was successfully created!'
     else
       render 'new'
     end
   end
 
   def edit
-    @comment = Comment.find(params[:id])
   end
 
   def update
-    @comment = Comment.find(params[:id])
+    @author = current_post_author
     if @comment.update(comment_params)
-      redirect_to current_user, notice: 'Comment was successfully updated!'
+      redirect_to @author, notice: 'Comment was successfully updated!'
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @comment.destroy
+    @author = current_post_author
+    redirect_to @author, notice: 'Comment was successfully deleted!'
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:content, :author_id, :commentable_id, :commentable_type)
+  end
+
+  def current_post_author
+    id = @comment.commentable_id
+    @author = @comment.commentable_type.constantize.find(id).author
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 end
