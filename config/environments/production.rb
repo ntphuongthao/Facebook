718b0605de50abbf3160474582cf0595097a2d79
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2022-07-04 11:05:32 +0700

    Changed production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "facebook_production"
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'peaceful-springs-19339.herokuapp.com' }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
